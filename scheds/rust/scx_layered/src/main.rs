ce5afef87eeec3e041cd4d63e853c78a505f420b
Daniel Hodges <hodges.daniel.scott@gmail.com>
2024-09-27 21:48:22 -0400

    cleanup

--- scheds/rust/scx_layered/src/main.rs
+++ scheds/rust/scx_layered/src/main.rs
@@ -1919,17 +1919,6 @@ impl<'a> Scheduler<'a> {
                 _ => {}
             }
         }
-        // for idx in 0..num_layers {
-        //     match self.layers[idx].kind {
-        //         LayerKind::Confined { .. }
-        //         | LayerKind::Grouped { .. }
-        //         | LayerKind::Open { .. } => {
-        //             // let layer = &self.layers[idx];
-        //             let mut layer = &mut self.skel.maps.bss_data.layers[idx];
-        //             layer.slice_ns =
-        //         }
-        //     }
-        // }
     }
 
     fn update_bpf_layer_cpumask(layer: &Layer, bpf_layer: &mut types::layer) {
